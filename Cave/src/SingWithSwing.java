f36f9cd77607bca8a46cb2a8b9e2634d39984af0
Benjamin Larson <benjamincrist8@gmail.com>
2014-06-27 18:15:37 -0500

    added versions of files to use Swing instead of console - work still in progress to convert them

---
+++ Cave/src/SingWithSwing.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * @author blarson
+ */
+public class SingWithSwing {
+    public static void main (String [] args) {
+        CatsInTheCradleLyricsWithSwing.singEntireSong();
+        CatsInTheCradleLyricsWithSwing.swingTest();
+
+    }
+}
