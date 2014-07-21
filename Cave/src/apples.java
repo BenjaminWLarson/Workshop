130a7ab46919c7afb15e6309cea14b5b6e4dc732
blarson <benjamincrist8@gmail.com>
2014-07-21 09:25:35 -0500

    playing with arrays and enhanced for loops

--- Cave/src/apples.java
+++ Cave/src/apples.java
@@ -12,21 +12,14 @@ class apples {
      * @args command line arguments
      */
     public static void main(String[] args) {
+       int myLilArray[] = {1,3,2,4,7,9};
 
-       Scanner input = new Scanner(System.in);
-       tuna myTunaObject = new tuna();
-
-       System.out.print("Enter name of first gf here: ");
-       String temp = input.nextLine();
-        myTunaObject.setName(temp);
-
-        myTunaObject.saying();
-
-
-
-
-
-
+        for (int i : myLilArray) {
+            System.out.println ("Number is: " + i);
+        }
+        for (int i = 0 ; i < myLilArray.length; i++) {
+            System.out.println ("Number is: " + myLilArray[i]);
+        }
     }
 }
 
