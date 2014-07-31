7325287d09bf3e328e40f958f8d81a77bdc75698
blarson <benjamincrist8@gmail.com>
2014-07-31 13:38:31 -0500

    fun with switch statement

---
+++ Cave/src/MarkI.java
@@ -0,0 +1,23 @@
+// This is a file to play
+
+public class MarkI {
+    public static void main(String[] args) {
+
+        System.out.println("welcome");
+        int someInt = 3;
+
+        switch (someInt) {
+            case 3:
+                System.out.println("case 3");
+            case 2:
+                System.out.println("case 2");
+            case 1:
+                System.out.println("case 1");
+            default:
+                System.out.println("end");
+                break;
+
+        }
+
+    }
+}
