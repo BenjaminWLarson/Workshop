581008126746b6572c47bb2d40c90942cbf56fbc
blarson <benjamincrist8@gmail.com>
2014-09-10 10:33:16 -0500

    fun with switch statement

--- Cave/src/MarkI.java
+++ Cave/src/MarkI.java
@@ -19,5 +19,43 @@ public class MarkI {
 
         }
 
+        int A = 5;
+        int B = 0;
+
+        if ((A > 0) || (B > 0)) {
+            System.out.println("OR || condition is true");
+        }
+        else {
+            System.out.println("OR || condition not happening");
+        }
+
+        if ((A > 0) | (B > 0)) {
+            System.out.println("OR | condition is true");
+        }
+        else {
+            System.out.println("OR | condition not happening");
+        }
+
+
+
+            boolean ready = false;
+            while (ready == false) {
+                System.out.println("waiting for 10 seconds things to settle down (pp, ss and ppm devices to go away)");
+                try {
+                    Thread.sleep(10 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                List<Pagepool> ppList = Pagepool.getBulk(getCompellent());
+//                List<SS> ssList = SS.getBulk(getCompellent());
+                int ppList = 1;
+                int ssList = 0;
+                if ((ppList > 0) || (ssList > 0)){
+                    ready = true;
+                    System.out.println(ready);
+                }
+            }
+
+
     }
 }
