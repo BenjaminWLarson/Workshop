73986c824efdeaad2282625161bcde11771e8383
blarson <benjamincrist8@gmail.com>
2014-10-15 10:38:04 -0500

    more playing

--- Cave/src/Sandbox.java
+++ Cave/src/Sandbox.java
@@ -1,48 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Sandbox {
 
     /**
      * @args command line arguments
      */
     public static void main(String[] args) {
-        for (int i = 8; i < 9; i++) {
-            System.out.println("some output...");
-        }
-
-        List<List<String>> listOfLists = new ArrayList<List<String>>();
-
-
-
-        ArrayList<String> blah = new ArrayList<String>();
-        blah.add("one");
-        blah.add("two");
-        blah.add("three");
-
-        ArrayList<String> blah2 = new ArrayList<String>();
-        blah.add("I");
-        blah.add("II");
-        blah.add("III");
-
-        ArrayList<String> blah3 = new ArrayList<String>();
-        blah.add("1");
-        blah.add("2");
-        blah.add("3");
-
-        listOfLists.add(blah);
-        listOfLists.add(blah2);
-        listOfLists.add(blah3);
 
 
 
 
+//        String temp = parser.getOffsetHex();
+        String temp = "1200010000000084";
 
-        System.out.println("printing the first thing here: " + blah.get(0));
-        System.out.println("printing the second thing here: " + blah.get(1));
-        System.out.println("printing the third thing here: " + blah.get(2));
+        System.out.println("temp is: " + temp);
+        System.out.println("string length is: " + temp.length());
+//        temp = temp.substring(7);
+        temp = temp.substring(1);
+        System.out.println("temp is: " + temp);
 
-        System.out.println("last element in second row is: " + listOfLists.get(0).get(2));
+
+        System.out.println ("trying the new thing....");
+//        logInfo("temp is: " + temp);
+//        BigInteger bigInt = new BigInteger(temp, 16);
+//        logInfo("bigInt is: " + bigInt);
+//        logInfo("now incrementing bigInt");
+//        bigInt.add(BigInteger.ONE);
+//        logInfo("bigInt is now: " + bigInt);
+//        logInfo("now turning bigInt back into a string");
+//        bigInt.toString();
+//        logInfo("bigInt is now: " + bigInt);
+
+
+//        int value = Integer.parseInt(temp, 16);
+//        value++;
+//        String incHex = Integer.toHexString(value);
+
+        Long hexSt = Long.valueOf(temp, 16).longValue();
+        hexSt++;
+        String blah = Long.toHexString(hexSt);
+//        blah.toString();
+
+        String blah2 = "00001"; 
+
+
+        blah = "1200010000000085";
+
+        System.out.println("now value is: " + blah);
+
+
+        String formatted = ("0000000000000000" + blah).substring(blah.length());
+
+//        String formatted = String.format("%05x", blah2);
+////        String formatted = String.format("")
+        System.out.println("now value is: " + formatted);
 
 
 
