8771377ac1c1429242e57864366c2fb5dad1d480
blarson <benjamincrist8@gmail.com>
2014-12-16 08:31:02 -0600

    more playing

--- Cave/src/net/benjaminlarson/AuxBrain.java
+++ Cave/src/net/benjaminlarson/AuxBrain.java
@@ -1,5 +1,9 @@
 package net.benjaminlarson;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 /**
@@ -15,6 +19,7 @@ public class AuxBrain {
     public static void main(String[] args) {
 
         System.out.println(getHostName());
+        WriteToFile(getHostName());
 
     }
 
@@ -26,6 +31,30 @@ public class AuxBrain {
             e.printStackTrace();
         }
         return i.getHostName();
+    }
+
+    public static void WriteToFile (String input) {
+        try {
+            File folder = new File("D:/AuxBrain/level1/level2");
+            String fileName = "test";
+            File file = new File(folder.getAbsolutePath() + "/" + fileName + ".txt");
+            folder.mkdirs();
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(input);
+            bw.close();
+
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
