a6700b575aeb07384abdfbe247cf4a990bc04092
blarson <benjamincrist8@gmail.com>
2015-04-03 13:05:27 -0500

    initial commit

---
+++ CaveArchive/src/Octocat.java
@@ -0,0 +1,13 @@
+/**
+ * Created by blarson on 4/3/2015.
+ */
+public class Octocat {
+
+    /**
+     * @args command line arguments
+     */
+    public static void main(String[] args) {
+        // Yeah, this is a file to play with git
+    }
+
+}
